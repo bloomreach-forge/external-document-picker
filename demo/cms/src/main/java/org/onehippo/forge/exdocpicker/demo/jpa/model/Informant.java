884ac15c35cbfd837717bcb8329408d5b378aa7d
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-03-27 11:08:27 +0200

    do all todo's after moving to GitHub: generate docs, add demo ('as is' from SVN trunk), remove refs to forge.onehippo.org

---
+++ demo/cms/src/main/java/org/onehippo/forge/exdocpicker/demo/jpa/model/Informant.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2015-2015 Hippo B.V. (http://www.onehippo.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onehippo.forge.exdocpicker.demo.jpa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "INFORMANT") 
+public class Informant implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "PHOTO")
+    private String photo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Informant)) {
+            return false;
+        }
+
+        if ((id == null && ((Informant) other).id == null) || (id.equals(((Informant) other).id))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return -1;
+        }
+
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Informant(id=" + id + ",name=" + name + ")";
+    }
+}
