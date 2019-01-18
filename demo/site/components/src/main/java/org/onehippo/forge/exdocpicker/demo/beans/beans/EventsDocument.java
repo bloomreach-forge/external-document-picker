e31c668b50241c2a019808b0e20db215f28f94b4
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-18 11:46:54 +0900

    HIPFORGE-243: recreating demo

---
+++ demo/site/components/src/main/java/org/onehippo/forge/exdocpicker/demo/beans/beans/EventsDocument.java
@@ -0,0 +1,112 @@
+package org.onehippo.forge.exdocpicker.demo.beans.beans;
+/*
+ * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:eventsdocument")
+@Node(jcrType="exdocpickerbasedemo:eventsdocument")
+public class EventsDocument extends HippoDocument {
+
+    /**
+     * The document type of the events document.
+     */
+    public static final String DOCUMENT_TYPE = "exdocpickerbasedemo:eventsdocument";
+
+    private static final String TITLE = "exdocpickerbasedemo:title";
+    private static final String DATE = "exdocpickerbasedemo:date";
+    private static final String INTRODUCTION = "exdocpickerbasedemo:introduction";
+    private static final String IMAGE = "exdocpickerbasedemo:image";
+    private static final String CONTENT = "exdocpickerbasedemo:content";
+    private static final String LOCATION = "exdocpickerbasedemo:location";
+    private static final String END_DATE = "exdocpickerbasedemo:enddate";
+
+    /**
+     * Get the title of the document.
+     *
+     * @return the title
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:title")
+    public String getTitle() {
+        return getProperty(TITLE);
+    }
+
+    /**
+     * Get the date of the document, i.e. the start date of the event.
+     *
+     * @return the (start) date
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:date")
+    public Calendar getDate() {
+        return getProperty(DATE);
+    }
+
+    /**
+     * Get the introduction of the document.
+     *
+     * @return the introduction
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:introduction")
+    public String getIntroduction() {
+        return getProperty(INTRODUCTION);
+    }
+
+    /**
+     * Get the image of the document.
+     *
+     * @return the image
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:image")
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean(IMAGE, HippoGalleryImageSet.class);
+    }
+
+    /**
+     * Get the main content of the document.
+     *
+     * @return the content
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    /**
+     * Get the location of the document.
+     *
+     * @return the location
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:location")
+    public String getLocation() {
+        return getProperty(LOCATION);
+    }
+
+    /**
+     * Get the end date of the document, i.e. the end date of the event.
+     *
+     * @return the end date
+     */
+    @HippoEssentialsGenerated(internalName = "exdocpickerbasedemo:enddate")
+    public Calendar getEndDate() {
+        return getProperty(END_DATE);
+    }
+
+}
