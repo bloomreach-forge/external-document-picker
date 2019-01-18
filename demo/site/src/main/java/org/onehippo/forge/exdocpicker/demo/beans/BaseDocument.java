659444863fd11ae5d0125b9c0c9a07341d74d7ca
Woonsan Ko <woonsan.ko@bloomreach.com>
2019-01-18 11:38:24 +0900

    HIPFORGE-243: remove old demo

--- demo/site/src/main/java/org/onehippo/forge/exdocpicker/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.exdocpicker.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="exdocpickerbasedemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
